71e589c5a63a275ff76591bfbac8b9ebbaba9aee
abdulazizsaidovcode <abdulazizsaidovcode@gmail.com>
2025-08-07 16:07:05 +0500

    some thing tested

--- test.logics.js
+++ test.logics.js
@@ -1,2 +1,10 @@
 
 Object.defineProperty()
+Object.keys(vnode.props).forEach(key => {
+    if (key.startsWith('on')) {
+        const eventName = key.slice(2).toLowerCase();
+        element.addEventListener(eventName, new Function(vnode.props[key]));
+    } else {
+        element.setAttribute(key, vnode.props[key]);
+    }
+});
